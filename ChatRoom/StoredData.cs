69d732a23d60df837b8377b2f7a98157c902be18
My Lilja <my.s.lilja@gmail.com>
2020-10-21 14:45:44 +0200

    First draft of functional locally storing data code

---
+++ ChatRoom/StoredData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ChatRoom
+{
+
+    /// <summary>
+    /// Stores the data from profile pages textboxes
+    /// </summary>
+    public class StoredData
+    {
+        public string _userNameBox;
+        public string _nameBox;
+        public string _cityBox;
+
+        public string userName
+        {
+            get { return _userNameBox; }
+            set { _userNameBox = value; }
+        }
+
+        public string name
+        {
+            get { return _userNameBox; }
+            set { _nameBox = value; }
+        }
+
+        public string city
+        {
+            get { return _cityBox; }
+            set { _cityBox = value; }        
+        }
+    }
+}
---
+++ ChatRoom/XmlFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ChatRoom
+{
+    /// <summary>
+    /// Creates and writes a XML file;
+    /// </summary>
+    class XmlFile
+    {
+        public static void writeXMLFile (object obj, string filename)
+        {
+            XmlSerializer sr = new XmlSerializer(obj.GetType());
+            TextWriter writer = new StreamWriter(filename);
+            sr.Serialize(writer, obj);
+            writer.Close();  
+        }
+    }
+}
