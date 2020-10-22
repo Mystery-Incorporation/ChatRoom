0d4b449c74883951b42b0319610a3bc91ef7b88f
Hampus Skogsberg <Skogsberg89@hotmail.com>
2020-10-22 10:16:55 +0200

    Code for localnetwork

---
+++ ChatRoom/Server/Server/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using ServerClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SocketServer mServer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            mServer = new SocketServer();
+        }
+
+        private void btnStartServer_Click(object sender, RoutedEventArgs e)
+        {
+            mServer.StartListeningForIncomingConnection();
+        }
+
+        private void btnSendAll_Click(object sender, RoutedEventArgs e)
+        {
+            mServer.SendToAll(txtMessage.Text.Trim());
+        }
+
+        private void btnStopServer_Click(object sender, RoutedEventArgs e)
+        {
+            mServer.StopServer();
+        }
+    }
+}
---
+++ ChatRoom/Server/ServerClassLibrary/SocketServer.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerClassLibrary
+{
+    public class SocketServer
+    {
+        IPAddress mIP;
+        int mPort;
+        TcpListener mTCPListener;
+
+        List<TcpClient> mClients;
+        public bool KeepRunning { get; set; }
+
+        public SocketServer()
+        {
+            mClients = new List<TcpClient>();
+        }
+
+        public async void StartListeningForIncomingConnection(IPAddress ipaddr = null, int port = 23000)
+        {
+            if (ipaddr == null)
+            {
+                ipaddr = IPAddress.Any;
+            }
+            if (port <= 0)
+            {
+                port = 23000;
+            }
+
+            mIP = ipaddr;
+            mPort = port;
+
+            System.Diagnostics.Debug.WriteLine(string.Format("IP Adress: {0} - Port: {1}", mIP.ToString(), mPort.ToString()));
+
+            mTCPListener = new TcpListener(mIP, mPort);
+
+            try
+            {
+                mTCPListener.Start();
+
+                KeepRunning = true;
+                while (KeepRunning)
+                {
+                    var returnedByAccept = await mTCPListener.AcceptTcpClientAsync();
+                    mClients.Add(returnedByAccept);
+                    Debug.WriteLine(string.Format("Client connected successfully, count: " + returnedByAccept.Client.RemoteEndPoint));
+
+                    TakeCareOfTCPClient(returnedByAccept);
+                }
+            }
+            catch (Exception excp)
+            {
+                System.Diagnostics.Debug.WriteLine(excp.ToString());
+            }
+        }
+
+        public void StopServer()
+        {
+            try
+            {
+                if (mTCPListener != null)
+                {
+                    mTCPListener.Stop();
+                }
+                foreach (TcpClient c in mClients)
+                {
+                    c.Close();
+                }
+                mClients.Clear();
+            }
+            catch (Exception excp)
+            {
+
+                Debug.WriteLine(excp.ToString());
+            }
+        }
+
+        private async void TakeCareOfTCPClient(TcpClient paramClient)
+        {
+            NetworkStream stream = null;
+            StreamReader reader = null;
+
+            try
+            {
+                stream = paramClient.GetStream();
+                reader = new StreamReader(stream);
+
+                char[] buff = new char[64];
+
+                while (KeepRunning)
+                {
+                    Debug.WriteLine("*** Ready to read");
+
+                    int nRent = await reader.ReadAsync(buff, 0, buff.Length);
+
+                    System.Diagnostics.Debug.WriteLine("Returned: " + nRent);
+
+                    if (nRent == 0)
+                    {
+                        RemoveClient(paramClient);
+                        System.Diagnostics.Debug.WriteLine("Socket disconnected");
+                        break;
+                    }
+
+                    string receivedText = new string(buff);
+                    System.Diagnostics.Debug.WriteLine("*** RECEIVED: " + receivedText);
+                    Array.Clear(buff, 0, buff.Length);
+
+                }
+            }
+            catch (Exception excp)
+            {
+                RemoveClient(paramClient);
+                System.Diagnostics.Debug.WriteLine(excp.ToString());
+            }
+        }
+
+        private void RemoveClient(TcpClient paramClient)
+        {
+            if (mClients.Contains(paramClient))
+            {
+                mClients.Remove(paramClient);
+                Debug.WriteLine(String.Format("Client removed, count: {0}", mClients.Count));
+            }
+        }
+
+        public async void SendToAll(string leMessage)
+        {
+            if (string.IsNullOrEmpty(leMessage))
+            {
+                return;
+            }
+            try
+            {
+                byte[] buffMessage = Encoding.UTF8.GetBytes(leMessage);
+
+                foreach (TcpClient c in mClients)
+                {
+                    c.GetStream().WriteAsync(buffMessage, 0, buffMessage.Length);
+                }
+            }
+            catch (Exception excp)
+            {
+                Debug.WriteLine(excp.ToString());
+            }
+        }
+    }
+}
